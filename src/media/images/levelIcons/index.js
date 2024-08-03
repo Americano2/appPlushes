7590eced17a1c115d27835b276dcc53df29767e3
Americano2 <viktorklishch6@gmail.com>
2024-08-03 15:15:11 +0300

    Delete media

--- src/media/images/levelIcons/index.js
+++
@@ -1,6 +0,0 @@
-import ThreeOrFour from "./3OR4.png"
-import OneJump from "./OneJump.png"
-import TwoJump from "./TwoJump.png"
-import ThreeJump from "./ThreeJump.png"
-
-export {ThreeOrFour, OneJump, TwoJump, ThreeJump}
--- src/media/images/photoPlushes/index.js
+++
@@ -1,9 +0,0 @@
-import TwitiPhoto from "./Twiti.jpeg"
-import PoohPhoto from "./Pooh.jpeg"
-import AnastasiaPhoto from "./Anastasia.png"
-
-export const photos = {
-    anastasia: AnastasiaPhoto,
-    twiti : TwitiPhoto,
-    pooh : PoohPhoto
-}
